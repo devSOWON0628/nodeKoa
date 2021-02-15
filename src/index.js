7560a328f0eadb6f1f3842a7d48c16d44acb0c26
parksowon <psw@gsitm.com>
2021-02-15 11:30:47 +0900

    초기 커밋

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+const api = require('./api');
 
-app.use((ctx, next) => {    // next는 promise를 반환
-    console.log(1);
-    next();
-});
+router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 
-
-app.use((ctx, next) => {
-    console.log(2);
-    next();
-});
-
-
-app.use((ctx, next) => {
-    ctx.body = 'Hello Koa';
-    next();
-});
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('heurm server is listening to port 4000');
